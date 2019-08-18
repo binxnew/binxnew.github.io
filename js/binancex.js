5d3a3db49cd9b9cdd750529abb16e4d15a5fe353
Teck <hiteck@teckbookpro2019.lan>
2019-08-18 10:41:22 -0700

    initial commit

---
+++ js/binancex.js
@@ -0,0 +1,6 @@
+var nav_template = Handlebars.templates['nav'];
+var nav_html = nav_template(nav_context);
+$(".b-navbar").html(nav_html);
+
+var footer_template = Handlebars.templates['footer'];
+$(".b-footer").html(footer_template);
