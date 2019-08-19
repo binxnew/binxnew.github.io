a72378530d505d9a72446231620ca58654aaaf2d
Teck <hiteck@TeckBookPro2019.local>
2019-08-19 09:06:30 -0700

    initial commit

--- js/fellows_list.js
+++ js/fellows_list.js
@@ -9,42 +9,51 @@ var fellowship_fellows_context = {
       {
             name:'Ivan Sorokin2', 
             img_url: './static/fellows/IronBelly_Ivan.png', 
-            project: 'IronBelly Grin Wallet'
+            project: 'IronBelly Grin Wallet',
+            detail_url: './fellow_ivan_sorokin.html'
+
       }, 
       {
             name:'Ivan Sorokin3', 
             img_url: './static/fellows/IronBelly_Ivan.png', 
-            project: 'IronBelly Grin Wallet'
+            project: 'IronBelly Grin Wallet',
+            detail_url: './fellow_ivan_sorokin.html'
       }, 
       {
             name:'Ivan Sorokin4', 
             img_url: './static/fellows/IronBelly_Ivan.png', 
-            project: 'IronBelly Grin Wallet'
+            project: 'IronBelly Grin Wallet',
+            detail_url: './fellow_ivan_sorokin.html'
       }, 
       {
             name:'Ivan Sorokin5', 
             img_url: './static/fellows/IronBelly_Ivan.png', 
-            project: 'IronBelly Grin Wallet'
+            project: 'IronBelly Grin Wallet',
+            detail_url: './fellow_ivan_sorokin.html'
       }, 
       {
             name:'Ivan Sorokin6', 
             img_url: './static/fellows/IronBelly_Ivan.png', 
-            project: 'IronBelly Grin Wallet'
+            project: 'IronBelly Grin Wallet',
+            detail_url: './fellow_ivan_sorokin.html'
       }, 
       {
             name:'Ivan Sorokin7', 
             img_url: './static/fellows/IronBelly_Ivan.png', 
-            project: 'IronBelly Grin Wallet'
+            project: 'IronBelly Grin Wallet',
+            detail_url: './fellow_ivan_sorokin.html'
       }, 
       {
             name:'Ivan Sorokin8', 
             img_url: './static/fellows/IronBelly_Ivan.png', 
-            project: 'IronBelly Grin Wallet'
+            project: 'IronBelly Grin Wallet',
+            detail_url: './fellow_ivan_sorokin.html'
       }, 
       {
             name:'Ivan Sorokin9', 
             img_url: './static/fellows/IronBelly_Ivan.png', 
-            project: 'IronBelly Grin Wallet'
+            project: 'IronBelly Grin Wallet',
+            detail_url: './fellow_ivan_sorokin.html'
       }, 
       ]
             
