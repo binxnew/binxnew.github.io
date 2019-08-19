ff47c2da95787b4ca8a7fd4fbec6a4a9985bced1
Teck <hiteck@teckbookpro2019.lan>
2019-08-18 19:26:31 -0700

    initial commit

--- js/binancex.js
+++ js/binancex.js
@@ -2,5 +2,10 @@ var nav_template = Handlebars.templates['nav'];
 var nav_html = nav_template(nav_context);
 $(".b-navbar").html(nav_html);
 
+var fellowship_tabs_template = Handlebars.templates['fellowship_tabs'];
+var fellowship_tabs_html = fellowship_tabs_template(fellowship_tabs_context);
+$("#fellowship_tabs_div").html(fellowship_tabs_html);
+
 var footer_template = Handlebars.templates['footer'];
 $(".b-footer").html(footer_template);
+
