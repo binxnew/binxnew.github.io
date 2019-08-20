930d97818c2a50e57fa03b4c91f117f8e15b357c
Teck <hiteck@teckbookpro2019.lan>
2019-08-20 07:19:43 -0700

    initial commit

---
+++ js/fellows_content/fellow_ivan_sorokin.js
@@ -0,0 +1,16 @@
+var fellowship_fellow_detail_context = {
+      project_name: 
+            "Burner Wallet",
+      fellow_name:
+            "Austin Griffith",
+      youtube_url: 
+            "https://www.youtube.com/embed/2Zfj_3T42Lw",
+      project_description:
+            "The Burner Wallet is a quick web wallet used to move small amounts of crypto quickly. On page load an Ethereum keypair is automatically generated and used to sign transactions with an incredibly simple interface. Sending funds is as easy as a QR code scan.", 
+      fellow_bio: 
+            "Austin Griffith is the everyday man of Ethereum. While he deeply respects the complexities of the technology, he views products through the lens of an average user. It\u2019s incredibly important that products built around this wonderful technology are actually usable and make sense. Some might argue that it is too early to focus on user experience, but Austin disagrees. His happy place is seeing a room full of crypto-initiated people smiling with disbelief while sending around crypto frictionless, participating in a prediction market for the first time, or playing a blockchain game with the tap of a finger. No barriers to entry, no downloads, no seed phrases in the sock drawer.", 
+      social_media_url:
+            "https://twitter.com/austingriffith",
+      social_media_handle:
+            "@austingriffith",
+}
