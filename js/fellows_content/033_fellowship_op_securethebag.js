c34856931a9e0d2492b747aa76c94d9f5ec182bd
Flora Sun <sunflora@gmail.com>
2019-08-28 07:44:40 +0200

    add video

--- js/fellows_content/033_fellowship_op_securethebag.js
+++ js/fellows_content/033_fellowship_op_securethebag.js
@@ -4,7 +4,7 @@ var fellowship_fellow_detail_context = {
       fellow_names:
             "Jeremy Rubin",
       youtube_url: 
-            "",
+            "https://www.youtube.com/embed/yHvyq-ZKb20",
       project_description:
             "Currently in Bitcoin, when a payment is made it is also received. This means that the transaction size is a function of the number of inputs spent and the number of users receiving. For a large business trying to make a large number of payments, this either means paying very high fees or having transactions get stuck confirming.  With OP_SECURETHEBAG, we separate out the concern of receiving and spending. Essentially, OP_SECURETHEBAG allows a payer to issue an unlimited number of Certified Checks (like the kind you get from the bank) which are fully and provably collateralized in a single UTXO. At any time later, these certified checks can be redeemed into outputs. This leads to better utilization of blockspace, a healthier backlog of transactions, and less time waiting for confirmations for users.", 
       fellows: [
